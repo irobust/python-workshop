9e807e1e54ab421fc5d5fb3429ba979007ba7015
Phanupong Permpimol <phanupong@irobust.co.th>
2023-09-18 14:59:34 +0700

    update try execpt

--- trycatch.py
+++ trycatch.py
@@ -2,14 +2,14 @@ def key_error():
     dict1 = dict(a=10, b=20)
     list1 = [1,2,3,4]
     try:
-        print(dict1['a'])
+        print(dict1['c'])
         print(list1[4])
-    except IndexError:
-        print("List1 doesn't has this index")
-    except KeyError:
-        print("Dict1 doesn't have this key")
-    except:
-        print("Generic error message")
+    except IndexError as e:
+        print(f"Error:{e}")
+    except KeyError as e:
+        print(f"Dict Error: {e}")
+    except Exception as e:
+        print(f"Generic error message: {e}")
     finally:
         print('Keep running program!')
 
@@ -21,7 +21,7 @@ def value_error():
         print('Invalid input, please input [0-9].')
 
 def main():
-    value_error()
+    key_error()
 
 if __name__ == "__main__":
     main()
