55f0a3a4535015d736f65bc21df3bcb2f1ef1f64
Phanupong Permpimol <phanupong@irobust.co.th>
2023-09-19 16:53:26 +0700

    update postgres example

--- database/postgres/main.py
+++ database/postgres/main.py
@@ -24,7 +24,23 @@ def new_investment(coin_id, currency, amount):
 
     print(f"Added investment for {coin_id} in {currency}")
 
+def show_investments():
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    sql = 'SELECT * FROM investments'
+
+    cursor.execute(sql)
+    investments = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    for investment in investments:
+        print(investment)
+
 def main():
     new_investment('bitcoin', 'thb', 1000)
+    show_investments()
 
 if __name__=='__main__': main()
