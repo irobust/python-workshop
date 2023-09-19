616f85312ef1ea708b369174c22532a39115e47f
Phanupong Permpimol <phanupong@irobust.co.th>
2023-09-19 16:41:24 +0700

    add postgres example

---
+++ database/postgres/main.py
@@ -0,0 +1,30 @@
+import psycopg
+
+def get_connection():
+    connection = psycopg.connect(
+        host = "localhost",
+        dbname = "postgres",
+        user = "postgres",
+        password = "mysecret"
+    )
+    return connection
+
+def new_investment(coin_id, currency, amount):
+    sql = f"""INSERT INTO investments(coin_id, currency, amount) 
+             VALUES('{coin_id}', '{currency}', {amount})
+          """
+    connection = get_connection()
+    cursor = connection.cursor()
+
+    cursor.execute(sql)
+    connection.commit()
+
+    cursor.close()
+    connection.close()
+
+    print(f"Added investment for {coin_id} in {currency}")
+
+def main():
+    new_investment('bitcoin', 'thb', 1000)
+
+if __name__=='__main__': main()
